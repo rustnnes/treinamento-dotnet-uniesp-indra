8283ae453d3ee4139967dad07dddde85a95ce33a
Diego Rustnnes Bandeira <rustnnes@gmail.com>
2021-05-23 22:55:37 -0300

    Formatting code

--- web/Startup.cs
+++ web/Startup.cs
@@ -14,23 +14,18 @@ namespace TreinamentoWebApp {
 
 		public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services) {
 			services.AddControllersWithViews();
 			services.AddScoped<ICarroServico<Carro>, CarroServico>();
 			services.AddScoped<IPaisServico<Pais>, PaisServico>();
-
-
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
 			if (env.IsDevelopment()) {
 				app.UseDeveloperExceptionPage();
 			}
 			else {
 				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
 			app.UseHttpsRedirection();
