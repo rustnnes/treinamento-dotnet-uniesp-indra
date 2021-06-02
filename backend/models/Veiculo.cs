a9d761440fe318bcf613c2eca4f0f790ef6ceb0e
Diego Rustnnes Bandeira <rustnnes@gmail.com>
2021-06-02 01:01:45 -0300

    Edited Veiculo model

--- backend/models/Veiculo.cs
+++ backend/models/Veiculo.cs
@@ -1,30 +1,34 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Concurrent;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Backend {
 	[Table("vehicles")]
 	public class Veiculo {
 		public Veiculo() { }
-		public Veiculo(string cor, string nome, int idMarca) {
+		public Veiculo(string cor, string nome, int idMarca, int ano, string tipo) {
 			this.cor = cor;
 			this.nome = nome;
 			this.idMarca = idMarca;
+			this.ano = ano;
+			this.tipo = tipo;
 		}
 
 		[Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public int id { get; set; }
 		public string nome { get; set; }
-		public string ano { get; set; }
+		public int ano { get; set; }
 		public string cor { get; set; }
 
-		[StringLength(50)]
+		[Required]
+		[StringLength(8, MinimumLength = 8)]
 		public string placa { get; set; }
+		public string tipo { get; set; }
 		public int idMarca { get; set; }
 
-		[ForeignKey("IdMarca")]
+		[ForeignKey(nameof(Veiculo.idMarca))] //"idMarca"
 		public virtual Marca marca { get; set; }
 		// http://thebillwagner.com/Blog/Item/2015-07-16-AC6gotchaInitializationvsExpressionBodiedMembers
-		public string tipo { get; } = "Carro";
 
 		// public string Buzinar() => "Biii";
 
